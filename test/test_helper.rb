3f1ec57fb9ba2130c3444dd25cbea57e6bab0438
Italo Paiva <italosensei@hotmail.com>
2017-08-18 14:26:15 -0300

    Configuring Travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 SimpleCov.start
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require 'sei'
 require 'byebug'
