2c568ce18e4517902d5ae9d5374c83727aa9c3d9
Italo Paiva <italosensei@hotmail.com>
2017-08-18 15:29:58 -0300

    Testing IncluirDocumento service

---
+++ test/servicos/incluir_documento_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+describe 'Sei::V3::Servicos::IncluirDocumento' do
+  before do
+    @wsdl = 'test URL'
+    @sigla_sei = 'SIGLA TESTE'
+    @identificacao = 'Identificação do serviço'
+    Sei.configure do |config|
+      config.wsdl = @wsdl
+      config.follow_redirects = true
+      config.pretty_print_xml = true
+      config.sigla = @sigla_sei
+      config.identificacao = @identificacao
+    end
+  end
+
+  it 'tem como classe de retorno a classe RetornoGeracaoProcedimento' do
+    assert_equal(
+      Sei::V3::Servicos::IncluirDocumento::RESPONSE_CLASS,
+      Sei::V3::Estruturas::RetornoInclusaoDocumento
+    )
+  end
+
+  it 'inclui um documento' do
+    documento = Sei::V3::Estruturas::Documento.new
+    documento.tipo = 'G'
+    documento.id_serie = '3'
+    documento.numero = '456453'
+    documento.descricao = 'Descrição do documento teste...'
+    documento.observacao = 'Observacao teste do documento'
+    documento.conteudo = 'Conteudo do documento'
+    documento.nivel_acesso = '0'
+    documento.id_procedimento = '155373'
+    id_unidade = '100091020'
+
+    # Mensagem esperada
+    msg = {
+      SiglaSistema: Sei.configuration.sigla,
+      IdentificacaoServico: Sei.configuration.identificacao,
+      IdUnidade: id_unidade,
+      Documento: documento.to_h
+    }
+
+    call_service :incluir_documento, expected_message: msg do
+      Sei::V3::Servicos::IncluirDocumento.call id_unidade, documento
+    end
+  end
+end
