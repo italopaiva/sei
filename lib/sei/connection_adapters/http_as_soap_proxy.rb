82973771f21fc8fcca72842a3a1b2d5ea96eed3a
Italo Paiva <italosensei@hotmail.com>
2017-10-16 11:32:44 -0200

    Removing facets string methods

--- lib/sei/connection_adapters/http_as_soap_proxy.rb
+++ lib/sei/connection_adapters/http_as_soap_proxy.rb
@@ -2,13 +2,12 @@ module Sei
   module ConnectionAdapters
     require 'http'
     require 'singleton'
-    require 'facets/string/camelcase'
 
     class HttpAsSoapProxy
       include Singleton
 
       def call(service, message, raw_response: nil)
-        service_name = service.to_s.camelcase
+        service_name = service.to_s.camelize(:lower)
         params = { service: service_name, data: message }
         response = HTTP.post Sei.configuration.http_soap_proxy_url, json: params
         return response if raw_response
